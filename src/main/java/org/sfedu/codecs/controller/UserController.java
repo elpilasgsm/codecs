ddeaa9baf74e1da592b6c6d69548c55a50be56fd
Dmitrii Zaporozhets <duzaporozhets@sfedu.ru>
2020-01-11 15:53:34 +0300

    spring security fix

--- src/main/java/org/sfedu/codecs/controller/UserController.java
+++ src/main/java/org/sfedu/codecs/controller/UserController.java
@@ -1,13 +1,16 @@
 package org.sfedu.codecs.controller;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.sfedu.codecs.model.db.UserEntity;
 import org.sfedu.codecs.model.dto.User;
 import org.sfedu.codecs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/users/")
@@ -24,6 +27,23 @@ public class UserController {
     @RequestMapping(value = "/", method = RequestMethod.PUT)
     public User put(@RequestBody User user) {
         return userService.create(user);
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/{user-id}", method = RequestMethod.GET)
+    public User get(@PathVariable("user-id") Long id, HttpServletResponse response) throws IOException {
+        UserEntity entity = userService.getById(id);
+        if (entity == null) {
+            response.sendError(HttpResponseStatus.NOT_FOUND.code());
+            return null;
+        } else {
+            User u = new User();
+            u.setId(entity.getUserId());
+            u.setLogin(entity.getLogin());
+            u.setStatus(entity.isActive() ? "ACTIVE" : "INACTIVE");
+            return u;
+        }
     }
 }
 
