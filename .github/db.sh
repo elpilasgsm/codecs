2929e2f32b33c37b1d9dd5d71563f80e6d5bee90
Dmitry Zaprozhets <dmitry.zaporozhec@openiam.com>
2020-01-24 15:42:18 +0300

    fix names

--- .github/db.sh
+++ .github/db.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 
-mysql -uroot -p $(mariaRootPassword) < data/1.users.SQL
-mysql -uroot -p $(mariaRootPassword) < data/2.RECORD.SQL
-mysql -uroot -p $(mariaRootPassword) < data/3.CHANGES.SQL
